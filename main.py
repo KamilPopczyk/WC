6cfb25f1088a517ca62431e29c29133f88ebabe6
Kamil <kam.popczyk@gmail.com>
2019-02-05 16:30:36 +0100

    First dict.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def site_map(url: str):
     site = requests.get(url).text
     html_parser = BeautifulSoup(site, 'html.parser')
     site_domain = url.split('//')[-1].split('//')[0]    # separate domain from url
+    site_map_dict = dict()
 
     print(site_domain)
     title = html_parser.find('title').text
@@ -15,14 +16,15 @@ def site_map(url: str):
     links_set = set()   # storage for links
     for link in links:
         new_link = link.get('href')     # cut only links
-        if new_link.startswith('/') \
-                or new_link.startswith('http://' + site_domain) \
-                or new_link.startswith('https://' + site_domain):    # filter links with domain
-
+        if new_link.startswith('/'):    # filter links with domain
+            links_set.add('http://' + site_domain + new_link)   # make correct url
+        if new_link.startswith('http://' + site_domain) or new_link.startswith('https://' + site_domain):
             links_set.add(new_link)
 
+    site_map_dict[url] = {'title': title,
+                          'links': links_set}
     # debuging print ;)
-    print(links_set)
+    print(site_map_dict)
 
 
 if __name__ == '__main__':
