3c5eaea9d077564beaf43bbef85189311c5a1c4f
Kamil <kam.popczyk@gmail.com>
2019-02-05 16:19:54 +0100

    Links.

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def site_map(url: str):
     site = requests.get(url).text
-    print(site)
     html_parser = BeautifulSoup(site, 'html.parser')
+    site_domain = url.split('//')[-1].split('//')[0]    # separate domain from url
+
+    print(site_domain)
     title = html_parser.find('title').text
     print(title)
+    links = html_parser.find_all('a')
+    print(links)
+    links_set = set()   # storage for links
+    for link in links:
+        new_link = link.get('href')     # cut only links
+        if new_link.startswith('/') \
+                or new_link.startswith('http://' + site_domain) \
+                or new_link.startswith('https://' + site_domain):    # filter links with domain
 
-site_map('http://0.0.0.0:8000')
+            links_set.add(new_link)
+
+    # debuging print ;)
+    print(links_set)
+
+
+if __name__ == '__main__':
+    site_map('http://0.0.0.0:8000')
